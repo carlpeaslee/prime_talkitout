1e6df47b2013540ba6c86b1295468911cafe65ea
Carl Peaslee <poodle@Carls-MacBook-Pro.local>
2016-02-24 08:51:36 -0600

    matt's additions

--- app1.js
+++ app1.js
@@ -37,3 +37,48 @@ purple = parseInt(purple);
 
 console.log("value :", value);
 console.log("purple : ", purple);
+
+// Switching over to Matt
+
+value = value + purple;
+
+console.log("Step 8:", value);
+
+if (value > 7352) {
+	value = 4;
+} else if (value == 6102) {
+	value = 54;
+} else {
+	value = 28;
+};
+
+console.log("Step 9:", value);
+
+var counter = 5;
+
+while (counter > 0) {
+	value++;
+	counter--;
+};
+
+console.log("Step 10:", value);
+
+function purpleFun(val) {
+	val = val.toString();
+	if (val.length == 1) {
+		value = val;
+		return val;
+
+	} else {
+		val = val.slice(1);
+		value = val;
+		return val;
+	};
+
+
+};
+
+purpleFun(value);
+
+//console.log("Steps 11 and 12:", value);
+console.log(value);
